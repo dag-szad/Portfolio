7fa65759e2e2af158ba2abfd8d45693f8d68c46f
Dag <dagmara.szadkowska@wp.pl>
2024-08-15 20:30:19 +0200

    feat: add theme toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './theme.js';
+
+import moonIcon from './assets/moon.svg';
+import sunIcon from './assets/sun.svg';
+
+const App = () => {
+  const [theme, setTheme] = useState('light');
+
+  const isDarkTheme = theme === 'dark';
+
+  const toggleTheme = () => {
+    const updatedTheme = isDarkTheme ? 'light' : 'dark';
+    setTheme(updatedTheme);
+    localStorage.setItem('theme', updatedTheme);
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    const prefersDark =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (savedTheme && ['dark', 'light'].includes(savedTheme)) {
+      setTheme(savedTheme);
+    } else if (prefersDark) {
+      setTheme('dark');
+    }
+  }, []);
+
   return (
-    <>
-    
-    </>
-  )
-}
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <>
+        <GlobalStyles />
+        <h1>Text test!</h1>
+        <button onClick={toggleTheme}>
+          {isDarkTheme ? (
+            <img src={sunIcon} alt="Light theme icon" />
+          ) : (
+            <img src={moonIcon} alt="Dark theme icon" />
+          )}
+        </button>
+      </>
+    </ThemeProvider>
+  );
+};
 
 export default App;
